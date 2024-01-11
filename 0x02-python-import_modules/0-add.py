f30f51e0cc723eb8011eec7fa504207e927ffd26
favoour <favour.fisayo@yahoo.com>
2024-01-10 21:18:24 -0800

    python

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,6 @@
-# Save this code in a file named main.py
-
-a = 1
-b = 2
-
+#!/usr/bin/python3
 from add_0 import add
-
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/variable_load_5.py
+++ 0x02-python-import_modules/variable_load_5.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
 a = 98
+
 """Simple variable
+
 """
