4ef60d270b01cbde0e88a8514252941cd2471f0d
favoour <favour.fisayo@yahoo.com>
2024-01-19 08:40:46 -0800

    loops

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+i#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+    """Match the bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
