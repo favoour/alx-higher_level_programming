d141495e7bba7562577239be6c993de3656387b6
favoour <favour.fisayo@yahoo.com>
2024-04-09 19:05:57 -0700

    javascript

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      // If w or h is zero or negative, create an empty object
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
